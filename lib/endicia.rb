e623a2f1b8c69c64d79c5b636a7c18beda9b00a9
jdejong <jdejong@sparknettech.com>
2011-05-25 10:35:27 -0400

    moved to using .present? instead of !.empty?

--- lib/endicia.rb
+++ lib/endicia.rb
@@ -35,28 +35,28 @@ module Endicia
 		Rails.logger.debug @default_options.inspect
 		request = XmlNode.new('LabelRequest', :Test => @default_options[:Test], :LabelSize => @default_options[:LabelSize], :ImageFormat => @default_options[:ImageFormat], :LabelType => @default_options[:LabelType], :ImageRotation => @default_options[:ImageRotation]) do |label_request|
           #label_request << XmlNode.new('Test', @default_options[:Test])
-		  label_request << XmlNode.new('AccountID', @default_options[:AccountID]) if @default_options.has_key?(:AccountID) and !@default_options[:AccountID].empty?
-          label_request << XmlNode.new('RequesterID', @default_options[:RequesterID]) if @default_options.has_key?(:RequesterID) and !@default_options[:RequesterID].empty?
-          label_request << XmlNode.new('PassPhrase', @default_options[:PassPhrase]) if @default_options.has_key?(:PassPhrase) and !@default_options[:PassPhrase].empty?
+		      label_request << XmlNode.new('AccountID', @default_options[:AccountID]) if @default_options.has_key?(:AccountID) and @default_options[:AccountID].present?
+          label_request << XmlNode.new('RequesterID', @default_options[:RequesterID]) if @default_options.has_key?(:RequesterID) and @default_options[:RequesterID].present?
+          label_request << XmlNode.new('PassPhrase', @default_options[:PassPhrase]) if @default_options.has_key?(:PassPhrase) and @default_options[:PassPhrase].present?
           
-		  label_request << XmlNode.new('FromName', @default_options[:FromName]) if @default_options.has_key?(:FromName) and !@default_options[:FromName].empty?
-          label_request << XmlNode.new('FromCompany', @default_options[:FromCompany]) if @default_options.has_key?(:FromCompany) and !@default_options[:FromCompany].empty?
-          label_request << XmlNode.new('ReturnAddress1', @default_options[:ReturnAddress1]) if @default_options.has_key?(:ReturnAddress1) and !@default_options[:ReturnAddress1].empty?
-          label_request << XmlNode.new('FromCity', @default_options[:FromCity]) if @default_options.has_key?(:FromCity) and !@default_options[:FromCity].empty?
-          label_request << XmlNode.new('FromState', @default_options[:FromState]) if @default_options.has_key?(:FromState) and !@default_options[:FromState].empty?
-          label_request << XmlNode.new('FromPostalCode', @default_options[:FromPostalCode]) if @default_options.has_key?(:FromPostalCode) and !@default_options[:FromPostalCode].empty?
+		      label_request << XmlNode.new('FromName', @default_options[:FromName]) if @default_options.has_key?(:FromName) and @default_options[:FromName].present?
+          label_request << XmlNode.new('FromCompany', @default_options[:FromCompany]) if @default_options.has_key?(:FromCompany) and @default_options[:FromCompany].present?
+          label_request << XmlNode.new('ReturnAddress1', @default_options[:ReturnAddress1]) if @default_options.has_key?(:ReturnAddress1) and @default_options[:ReturnAddress1].present?
+          label_request << XmlNode.new('FromCity', @default_options[:FromCity]) if @default_options.has_key?(:FromCity) and @default_options[:FromCity].present?
+          label_request << XmlNode.new('FromState', @default_options[:FromState]) if @default_options.has_key?(:FromState) and @default_options[:FromState].present?
+          label_request << XmlNode.new('FromPostalCode', @default_options[:FromPostalCode]) if @default_options.has_key?(:FromPostalCode) and @default_options[:FromPostalCode].present?
 		  
-          label_request << XmlNode.new('ToPostalCode', @default_options[:ToPostalCode]) if @default_options.has_key?(:ToPostalCode) and !@default_options[:ToPostalCode].empty?
-          label_request << XmlNode.new('ToName', @default_options[:ToName]) if @default_options.has_key?(:ToName) and !@default_options[:ToName].empty?
-          label_request << XmlNode.new('ToCompany', @default_options[:ToCompany]) if @default_options.has_key?(:ToCompany) and !@default_options[:ToCompany].empty?
-          label_request << XmlNode.new('ToAddress1', @default_options[:ToAddress1]) if @default_options.has_key?(:ToAddress1) and !@default_options[:ToAddress1].empty?
-		  label_request << XmlNode.new('ToAddress2', @default_options[:ToAddress1]) if @default_options.has_key?(:ToAddress2) and !@default_options[:ToAddress2].empty?
-          label_request << XmlNode.new('ToCity', @default_options[:ToCity]) if @default_options.has_key?(:ToCity) and !@default_options[:ToCity].empty?
-          label_request << XmlNode.new('ToState', @default_options[:ToState]) if @default_options.has_key?(:ToState) and !@default_options[:ToState].empty?
-          label_request << XmlNode.new('PartnerTransactionID',@default_options[:PartnerTransactionID]) if @default_options.has_key?(:PartnerTransactionID) and !@default_options[:PartnerTransactionID].empty?
-          label_request << XmlNode.new('PartnerCustomerID', @default_options[:PartnerCustomerID]) if @default_options.has_key?(:PartnerCustomerID) and !@default_options[:PartnerCustomerID].empty?
-          label_request << XmlNode.new('MailClass', @default_options[:MailClass]) if @default_options.has_key?(:MailClass) and !@default_options[:MailClass].empty?
-          label_request << XmlNode.new('WeightOz', @default_options[:WeightOz]) if @default_options.has_key?(:WeightOz) and !@default_options[:WeightOz].empty?
+          label_request << XmlNode.new('ToPostalCode', @default_options[:ToPostalCode]) if @default_options.has_key?(:ToPostalCode) and @default_options[:ToPostalCode].present?
+          label_request << XmlNode.new('ToName', @default_options[:ToName]) if @default_options.has_key?(:ToName) and @default_options[:ToName].present?
+          label_request << XmlNode.new('ToCompany', @default_options[:ToCompany]) if @default_options.has_key?(:ToCompany) and @default_options[:ToCompany].present?
+          label_request << XmlNode.new('ToAddress1', @default_options[:ToAddress1]) if @default_options.has_key?(:ToAddress1) and @default_options[:ToAddress1].present?
+		      label_request << XmlNode.new('ToAddress2', @default_options[:ToAddress2]) if @default_options.has_key?(:ToAddress2) and @default_options[:ToAddress2].present?
+          label_request << XmlNode.new('ToCity', @default_options[:ToCity]) if @default_options.has_key?(:ToCity) and @default_options[:ToCity].present?
+          label_request << XmlNode.new('ToState', @default_options[:ToState]) if @default_options.has_key?(:ToState) and @default_options[:ToState].present?
+          label_request << XmlNode.new('PartnerTransactionID',@default_options[:PartnerTransactionID]) if @default_options.has_key?(:PartnerTransactionID) and @default_options[:PartnerTransactionID].present?
+          label_request << XmlNode.new('PartnerCustomerID', @default_options[:PartnerCustomerID]) if @default_options.has_key?(:PartnerCustomerID) and @default_options[:PartnerCustomerID].present?
+          label_request << XmlNode.new('MailClass', @default_options[:MailClass]) if @default_options.has_key?(:MailClass) and @default_options[:MailClass].present?
+          label_request << XmlNode.new('WeightOz', @default_options[:WeightOz]) if @default_options.has_key?(:WeightOz) and @default_options[:WeightOz].present?
         end
 		Rails.logger.debug request.to_s
 		body = "labelRequestXML=" + request.to_s
